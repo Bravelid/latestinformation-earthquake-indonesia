9d8e84966278b53f6a7497c33f85afacd1098421
ADMIN <muhammadmaulanabdz@gmail.com>
2022-05-11 13:16:46 +0700

    0.4

--- README.md
+++ README.md
@@ -6,3 +6,11 @@
 <h3>This package can scrape from bmkg(bmkg.go.id)</h3> 
 <br>
 <b>This package uses technology from beautifulsoup4 and requests</b>
+
+## HOW TO USE?
+```
+if __name__ == "__main__":
+    print("Latest earthquake information")
+    result = ekstrak()
+    showdata(result)
+```
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="IndonesiaEarthquakeinfo",
-    version="0.2",
+    version="0.3",
     author="Muhammad Maulana B.",
     author_email="muhammadmaulanabdz@gmail.com",
     description="This package informs about the latest earthquake from Meteorology Climatology and "
