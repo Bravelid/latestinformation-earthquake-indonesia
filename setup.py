e8bf9e1744e6ada4eca5465bead712da64834c10
ADMIN <muhammadmaulanabdz@gmail.com>
2022-05-11 12:55:59 +0700

    0.2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         "Github": "https://github.com/Bravelid/latestinformation-earthquake-indonesia",
     },
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
 
